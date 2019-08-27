2dd2afb59ae053cdcb9ab8e1a4428bd57ddaecc0
mimicl10 <mirellacunha_lira@hotmail.com>
2019-08-27 15:25:04 -0300

    Verificando Aproximacao do Animal-FASE1

--- CaixaComportamental.py
+++ CaixaComportamental.py
@@ -9,4 +9,15 @@ elif animal_habituado == 1:
 else: 
     print("COMANDO INVÁLIDO... TENTE NOVAMENTE...")
 
-print("Qualquer coisa")
+primeira_fase = 0 #Variável de controle (passou da primeira fase ou não)
+if habituado == 1:
+    comprimento_caixa = 30
+    aproximacao_animal = float(input("QUANTOS CENTÍMETROS O ANIMAL SE APROXIMOU? "))
+    
+    if aproximacao_animal > 0 and aproximacao_animal <= comprimento_caixa:
+        print("LIBERAR 0.5ML DE RECOMPENSA...")
+        primeira_fase = 1
+    elif aproximacao_animal == 0:
+        print("NÃO LIBERAR RECOMPENSA...")
+    else:
+        print("VALOR INVÁLIDO...")
